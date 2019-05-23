ae3d497dc52ac9fb689ec48692c6d9bfcbaf8918
Tamas Korodi <tkorodi@me.com>
2019-05-23 22:17:06 +0200

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-serve "0.1.0"
+(defproject re-serve "0.1.1-SNAPSHOT"
   :description "re-frame like server side state handnling."
   :url "https://github.com/kotyo/re-serve"
   :license {:name "EPL-2.0"
